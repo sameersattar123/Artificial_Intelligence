f904535e9b68717354f594edb5b44166929e86df
Sameer <sameersattar1111@gmail.com>
2024-01-23 15:40:50 +0500

    variables

---
+++ Basics/variables.py
@@ -0,0 +1,18 @@
+name = "sameer" # string type  
+age = 44       # number type
+
+print(name)
+print(age)
+
+name = "ali"
+age  = 443 
+
+print(name)
+print(age)
+
+age  = 44.3   # float type
+print(age)
+
+is_adult = True  # boolean
+is_adult = False  # boolean
+print(is_adult)
