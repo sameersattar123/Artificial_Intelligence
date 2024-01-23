2bfcba29836fe8d90ca43e61fa8662613f73a3c0
Sameer <sameersattar1111@gmail.com>
2024-01-23 15:55:08 +0500

    variables and type conversions

---
+++ Basics/Input.py
@@ -0,0 +1,19 @@
+# taking input from user 
+
+firstName = input("What is your first name")
+lastName = input("What is your last name")
+
+name = firstName + " " + lastName
+
+print(name)
+
+# type conversions 
+
+num1 = input(" Enter the value of num1")
+
+num2 = input(" Enter the value of num1")
+
+totalSum = int(num1) + int(num2)  # convert string into integer
+
+print("The total sum of " + str(totalSum)) # convert integer into string
+
