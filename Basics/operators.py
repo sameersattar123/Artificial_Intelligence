e443c2e16bf0df6c0133b0713316bffafbed3fa1
Sameer <sameersattar1111@gmail.com>
2024-01-24 12:28:31 +0500

    logical operators

---
+++ Basics/operators.py
@@ -0,0 +1,33 @@
+# Arithmetic Operators
+sumOfTwoNums = 5 + 5
+print(sumOfTwoNums)
+
+subOfTwoNums = 5 - 5
+print(subOfTwoNums)
+
+multiplyOfTwoNums = 5 * 5
+print(multiplyOfTwoNums)
+
+divideOfTwoNums = 5 / 5
+print(divideOfTwoNums)
+
+num1 = 5 // 5 
+print(num1) # 1
+
+num2 = 5 ** 5 
+print(num2)  # 3125
+
+# Logical Operators
+print( 2 > 3  or  2 < 3)
+print( 2 < 3  and  2 < 3)
+print( not 2 < 3)
+
+
+# Comparison Operators
+print( 3 == 3 )
+print( 3 > 3 )
+print( 3 >= 3 )
+print( 3 < 3 )
+print( 3 <= 3 )
+print( 3 != 3 )
+print( 3 != 3 )
