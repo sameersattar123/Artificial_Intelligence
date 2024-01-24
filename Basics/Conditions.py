1eb35c841b431f178dcaa6e8a6a4d6afbaef57ec
Sameer <sameersattar1111@gmail.com>
2024-01-24 15:13:35 +0500

    conditions

---
+++ Basics/Conditions.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age"))
+
+if age >= 18: 
+    print("You are eligible for higher education")
+elif age < 18  and age > 3:
+    print("You are eligible for School education")
+else :
+    print("You are a child")
