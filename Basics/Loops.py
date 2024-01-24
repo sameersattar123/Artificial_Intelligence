17dd3a1e9045c0d52c1e8ca1e319aec75aa88a07
Sameer <sameersattar1111@gmail.com>
2024-01-24 15:44:48 +0500

    loops

---
+++ Basics/Loops.py
@@ -0,0 +1,22 @@
+# while loop
+
+i = 1
+while i <= 100:
+    print(i)
+    i = i + 1
+
+i = 1
+while i <= 5:
+    print(i * " * ")
+    i = i + 1
+
+i = 5
+while i >= 1:
+    print(i * " * ")
+    i = i - 1
+
+
+# for loop
+for item in range(5):
+    print(item + 1)
+
