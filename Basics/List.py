7911ef1919fd6574d0fc604309374add4534ccda
Sameer <sameersattar1111@gmail.com>
2024-01-25 10:15:15 +0500

    list tuple dictonary set

--- Basics/List.py
+++ Basics/List.py
@@ -19,5 +19,40 @@ while i < len(myMarks):
     print(myMarks[i])
     i = i + 1
 
+for item in myMarks:
+    print(item)
+    i = i + 1
+
 myMarks.clear()
 print(myMarks)
+
+#Tuple
+UniMarks = (44 , 55 , 66  , 66 ,66 ,66)
+# UniMarks[0] = 33 'tuple' object does not support item assignment
+print(UniMarks.count(66)) # count the number which is repeat in tuple by using tuple method
+print(UniMarks.index(55)) # identify the number position in tuple by using index method 
+print(UniMarks.index(66)) # identify the number position in tuple by using index method 
+print(UniMarks)
+
+# Set 
+UniqueElements = {3,4,4 ,4,4,4,44,45,6,7,8,8,8,8}
+# UniqueElements[0] = 33 #'set' object does not support item assignment
+print(UniqueElements)
+
+for marks in UniqueElements:
+    print(marks) # order does not matter in set but they are unique 
+
+# Dictionary
+
+information = {
+    "fatherName" : "Abdul Sattar",
+    "MotherName" : "Khalida Sattar",
+    "BrotherName" : "Daniyal Sattar",
+    "SitterName" : "Paras Sattar",
+} 
+
+print(information["fatherName"])
+print(information["MotherName"])
+information["SitterName"] = "Paras"
+print(information)
+
