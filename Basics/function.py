c69b983cdf17990f758b7e56116b4610d8a32045
Sameer <sameersattar1111@gmail.com>
2024-01-25 11:27:14 +0500

    functions

---
+++ Basics/function.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+
+def NumSqrt(num):
+    print(sqrt(num))
+
+NumSqrt(16)
+
+def TwoNumSum(num1 , num2):
+    result = num1 + num2
+    return result
+
+print(TwoNumSum(5,5))
+
+
+def ThreeNumSum(num1 , num2 , num3):
+    result = num1 + num2 + num3
+    print(result)
+
+
+ThreeNumSum(7,7,7)
