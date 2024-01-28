324b4772db6a723f40057ffc891e61a35d2e4703
Sameer <sameersattar1111@gmail.com>
2024-01-28 20:36:22 +0500

    lab 01 complete

--- Labs/Lab01/Lab01.py
+++ Labs/Lab01/Lab01.py
@@ -63,3 +63,13 @@
 
 # print(lowercase_long_words)
 
+# (ii)Write a Python program to print a specified list after removing the 0th, 4th and 5th elements Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow',’Teapink’] Expected Output : ['Green', 'White', 'Black']
+
+# Sample_List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow',"Teapink"]
+# Sample_List.remove('Red')
+# Sample_List.remove('Pink')
+# Sample_List.remove('Yellow')
+# Sample_List.remove('Teapink')
+# print(Sample_List)
+
+
