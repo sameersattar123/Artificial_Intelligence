2f75253386eacf02306fcca17c4b7377735b11f8
Sameer <sameersattar1111@gmail.com>
2024-01-28 20:21:21 +0500

    lab01

---
+++ Labs/Lab01/Lab01.py
@@ -0,0 +1,56 @@
+# Exercise Python input /output Basic operations
+
+# (i) Write a Python program to swap 4 variables values (input four values
+
+# a = int(input("Enter the value of a"))
+# b = int(input("Enter the value of b"))
+# c = int(input("Enter the value of c"))
+# d = int(input("Enter the value of d"))
+
+# print("Before swapping")
+# print(f"a={a} , b={b} , c={c} , d={d}")
+
+# temp1 = a 
+# a = d
+# d = temp1
+
+# temp2 = b
+# b = c 
+# c = temp2
+
+# print("After swapping")
+# print(f"a={a} , b={b} , c={c} , d={d}")
+
+# (ii) Write a Python program to convert temperatures to and from celsius,Fahrenheit.
+
+# def celsiusToFahrenheit(celsius):
+#     return (celsius * 9/5) + 32
+# def FahrenheitToCelsius(fahrenheit):
+#     return (fahrenheit - 32) * 5/9
+
+# celsius_input = float(input("Enter the temperature in Celsius"))
+# fahrenheit_Output  = celsiusToFahrenheit(celsius_input)
+# print(f"Temperature in Fahrenheit is {fahrenheit_Output}F")
+
+# fahrenheit_input = float(input("Enter the temperature in Celsius"))
+# celsius_Output  = FahrenheitToCelsius(fahrenheit_input)
+# print(f"Temperature in Fahrenheit is {celsius_Output}C")
+
+# Exercise: Dictionaries
+
+# (ii)Write a Python script to concatenate following dictionaries to create a new one.
+
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+
+# result_dic = {}
+
+# result_dic.update(dic1)
+# result_dic.update(dic2)
+# result_dic.update(dic3)
+
+# print("Expected Result:",result_dic)
+
+
+
