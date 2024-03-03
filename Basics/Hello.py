2382042505038475a21e83fd4458ea6300cd1a5b
Sameer <sameersattar1111@gmail.com>
2024-03-03 23:28:29 +0500

    modules

--- Basics/Hello.py
+++ Basics/Hello.py
@@ -1,1 +1,14 @@
+import mymodule
+
 print("sameer sattar shaikh")
+
+mymodule.greeting("sameer sattar shaikh")
+
+a = mymodule.person1['age']
+print(a)
+a = mymodule.person1['name']
+print(a)
+a = mymodule.person1['country']
+print(a)
+a = mymodule.person1
+print(a)
