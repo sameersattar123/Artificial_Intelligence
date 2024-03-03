13312299794ea667f1e7d526f47706d23b892532
Sameer <sameersattar1111@gmail.com>
2024-03-03 22:04:30 +0500

    Arrays and lambda

---
+++ Basics/Arrays.py
@@ -0,0 +1,20 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+print(cars)
+
+
+# Modify the value of the first array item
+cars[0] = "Audi" 
+
+# You refer to an array element by referring to the index number
+y = cars[0]
+
+print(y)
+
+# Use the len() method to return the length of an array (the number of elements in an array).
+z = len(cars)
+print(z)
+
+# You can use the for in loop to loop through all the elements of an array
+for i in cars:
+    print(i)
---
+++ Basics/Lambda.py
@@ -0,0 +1,11 @@
+# A lambda function that adds 10 to the number passed in as an argument, and print the result:
+x = lambda a: a + 10
+print(x(5))
+
+# A lambda function that multiplies argument a with argument b and print the result:
+y = lambda a,b: a*b
+print(y(5,5))
+
+# A lambda function that sums argument a, b, and c and print the result:
+z =  lambda a,b,c: a+b+c
+print(z(1,2,3))
